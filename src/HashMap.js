3dc9e47a57526e0a3c327fa7db3c5de5ff4c867e
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-04 14:14:13 +0800

    feat: add hash function

--- src/HashMap.js
+++ src/HashMap.js
@@ -18,4 +18,15 @@ export default class HashMap {
     this.capacity = initialCapacity;
     this.hashTable = new Array(initialCapacity).fill(null);
   }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
 }
